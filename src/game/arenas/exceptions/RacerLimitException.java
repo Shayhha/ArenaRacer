10a5b0d5d4a6b8027e72fd05a0abce0669693755
Shay Hahiashvili <shayhh@outlook.com>
2023-04-02 21:53:24 +0300

    ok

--- src/game/arenas/exceptions/RacerLimitException.java
+++ src/game/arenas/exceptions/RacerLimitException.java
@@ -1,13 +1,24 @@
 package game.arenas.exceptions;
 import game.racers.Racer;
 
+/**
+*RacerLimitException class extendes Exception class
+* includes constractor with our customised string 
+* class includes getMassage that we use in program
+* <p>
+*@param message - represetns our customized message we want the exception to throw
+*/
 public class RacerLimitException extends Exception {
     private String message;
 
-    public RacerLimitException(Racer r){
+    public RacerLimitException(Racer r){ //ctor
         this.message= "Arena is full! (" + r.getArena().getMAX_RACERS()+ " active racers exist). racer "+ r.getSerialNumber()+ " was not added";
     }
 
+    /**
+     * getMassage method
+     * @return message parameter
+     */
     public String getMessage(){
         return this.message;
     }
--- src/game/arenas/exceptions/RacerTypeException.java
+++ src/game/arenas/exceptions/RacerTypeException.java
@@ -1,13 +1,23 @@
 package game.arenas.exceptions;
 import game.racers.*;
 
+/**
+*RacerTypeException class extendes Exception class
+* includes constractor with our customised string 
+* class includes getMassage that we use in program
+* <p>
+*@param message - represetns our customized message we want the exception to throw
+*/
 public class RacerTypeException extends Exception{
     private String message;
 
     public RacerTypeException(Racer r){
         this.message= "Invalid Racer of type \""+ r.className() +"\" for "+r.getArena().getClass().getSimpleName()+ ".";
     }
-
+     /**
+     * getMassage method
+     * @return message parameter
+     */
     public String getMessage(){
         return this.message;
     }
