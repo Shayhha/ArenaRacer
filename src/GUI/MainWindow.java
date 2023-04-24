8cdc887582fa8a7a9e7d7a5180133e18bdf0c56e
Maxim Subotin <maxim.sub21@gmail.com>
2023-04-24 10:42:53 +0300

    addings some simple function and input tests

--- src/GUI/MainWindow.java
+++ src/GUI/MainWindow.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 
 public class MainWindow implements ActionListener {
 
@@ -12,15 +13,23 @@ public class MainWindow implements ActionListener {
     final static String[] RACERS = { "Airplain", "Helicopter", "Bicycle", "Car", "Horse", "RowBoat", "SpeedBoat"};
     final static String[] COLORS = {"Black", "Blue", "Green", "Red", "Yellow"};
 
+    private int maxNumOfRacers = 8; // values must be between 1-20
+    private int arenaLen = 1000; // values must be between 100-3000
+
     // Constant Values for the Screen Dimentions
     final static int MAIN_WINDOW_WIDTH = 1200;
-    final static int MAIN_WINDOW_HEIGHT = 728;
+    final static int MAIN_WINDOW_HEIGHT = 728; // when we have more that 11 racers on the screen we need to increase the height by RACER_ICON_SIZE
 
     final static int LEFT_PANEL_WIDTH = 1000;
     final static int LEFT_PANEL_HEIGHT = 700;
 
     final static int RIGHT_PANEL_WIDTH = 183;
     final static int RIGHT_PANEL_HEIGHT = 700;
+
+    final static int RACER_ICON_SIZE = 60; // width = height = 60
+
+    // ...
+    ArrayList<JLabel> racersList = new ArrayList<JLabel>(maxNumOfRacers);
 
     // Our Components:
     JLabel comboLabel = new JLabel("Choose arena:");
@@ -44,7 +53,7 @@ public class MainWindow implements ActionListener {
     JButton startRace = new JButton("Start race");
     JButton showInfo = new JButton("Show info");
     JFrame mainFrame = new JFrame("Race Game - Advanced OOP");
-    JPanel leftPanel = new JPanel();
+    JPanel leftPanel = new JPanel(null);
     JSeparator separator1 = new JSeparator(SwingConstants.HORIZONTAL);
     JSeparator separator2 = new JSeparator(SwingConstants.HORIZONTAL);
     JLabel backgroundLabel = new JLabel("");
@@ -178,6 +187,50 @@ public class MainWindow implements ActionListener {
         return p1;
     }
 
+    /**
+     * creating a single racer buy getting the icon from the given path and makeing a label that will hold that icon
+     * @param path the path to the icon we want to give the racer
+     * @return a JLabel object (an icon) that represents a racer in our race
+     */
+    private JLabel createRacer(String path) {
+        Image image = new ImageIcon(path).getImage().getScaledInstance(RACER_ICON_SIZE, RACER_ICON_SIZE, Image.SCALE_SMOOTH);
+        JLabel racer = new JLabel("", new ImageIcon(image), JLabel.CENTER);
+        racer.setBounds(5, 5, RACER_ICON_SIZE, RACER_ICON_SIZE);
+        return racer;
+    }
+
+    /**
+     * moves the given racer by adding to his X and Y coordinates
+     * @param racer the racer you want to move
+     * @param x how much to move on the X axies, 0 is dont move.
+     * @param y how much to move on the Y axies, 0 is dont move.
+     */
+    private void moveRacer(JLabel racer, int x, int y) {
+        racer.setLocation(racer.getX() + x, racer.getY() + y);
+    }
+
+    private boolean checkArenaLength(int len) {
+        if (len < 100 || len > 3000) {
+            JOptionPane.showMessageDialog(null,
+             "The length of the arena must be between 100 and 3000 units, try again.", "Arena Length Input Error",
+             JOptionPane.INFORMATION_MESSAGE //check is this is a good icon ( https://stackoverflow.com/questions/6562847/joptionpane-change-the-icon )
+             );
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkMaxRacers(int num) {
+        if (num < 1 || num > 20) {
+            JOptionPane.showMessageDialog(null,
+             "An arena can have between 1 and 20 racers, try setting a different maximum number or racers.", "Maximum Racers Input Error",
+             JOptionPane.INFORMATION_MESSAGE //check is this is a good icon ( https://stackoverflow.com/questions/6562847/joptionpane-change-the-icon )
+             );
+            return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         // creating the main window using some variables declared above
         MainWindow window = new MainWindow();
@@ -187,19 +240,30 @@ public class MainWindow implements ActionListener {
         
         // setting the correct layout to the frame and the panels
         mainFrame.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
+        mainFrame.setSize(MAIN_WINDOW_WIDTH,MAIN_WINDOW_HEIGHT);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // get the screen size
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+        // calculate the new location of the frame
+        int x = (int) ((screenSize.getWidth() - mainFrame.getWidth()) / 2);
+        int y = (int) ((screenSize.getHeight() - mainFrame.getHeight()) / 2);
+
+        // set the location of the frame
+        mainFrame.setLocation(x, y);
+
 
         leftPanel.setBorder(BorderFactory.createEmptyBorder(0,0,0,0));  
         leftPanel.setPreferredSize(new Dimension(LEFT_PANEL_WIDTH, LEFT_PANEL_HEIGHT));
         leftPanel.setBackground(Color.GRAY); // this is temporary
-        //leftPanel.setLayout(new BorderLayout()); // this might improve the positioning of the image in the left panel
+        leftPanel.setLayout(new BorderLayout()); // this might improve the positioning of the image in the left panel
 
         // adding the panels to the frame
         mainFrame.add(leftPanel);
         mainFrame.add(rightPanel, BorderLayout.NORTH);
 
         // setting the frame's size and making it visible
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainFrame.setSize(MAIN_WINDOW_WIDTH,MAIN_WINDOW_HEIGHT);
         mainFrame.setVisible(true);
     }
 
@@ -207,12 +271,32 @@ public class MainWindow implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == this.buildBtn) { // if the build arena button was clicked
             leftPanel.remove(backgroundLabel); // removing the existig image from the left panel before adding a new one
-
+            
             // getting the user's choises from the combo box and text boxes on the screen
             String arenaImagePath = "src/icons/" + (String)this.chooseArena.getSelectedItem() + ".jpg"; // creating the path to the background image of the area useing the choise from the user's input to the combo box
-            String arenaLength = this.arenaLength.getText();
-            String maxRacers = this.maxRacers.getText();
+            
+            // checking the input to of the user for arena
+            if (!this.arenaLength.getText().isEmpty()) {
+                arenaLen = Integer.parseInt(this.arenaLength.getText()); // need to add checks for this value
+                if (!checkArenaLength(arenaLen)) {
+                    return;
+                }
+                
+            } else { // this error message is not like the Word document, fix it.
+                JOptionPane.showMessageDialog(null,
+                 "Please fill in the \"Arena Length\" and \"Max racers Number\" fields before creating the arena.", "Missing Values Error",
+                 JOptionPane.INFORMATION_MESSAGE //check is this is a good icon ( https://stackoverflow.com/questions/6562847/joptionpane-change-the-icon )
+                );
+                return;
+            }
+            
+            if (!this.maxRacers.getText().isEmpty()) {
+                maxNumOfRacers = Integer.parseInt(this.maxRacers.getText()); // need to add checks for this value
 
+                if (!checkMaxRacers(maxNumOfRacers)) {
+                    return;
+                }
+            }
             // creating the background image of the arena with the path that is made of the user's choise
             ImageIcon icon = new ImageIcon(arenaImagePath);
             Image image = icon.getImage().getScaledInstance(leftPanel.getWidth(), leftPanel.getHeight(), Image.SCALE_SMOOTH); // setting the size of the image to be the size of the panel it will sit in
@@ -220,6 +304,20 @@ public class MainWindow implements ActionListener {
             // adding the background image to the screen
             backgroundLabel = new JLabel("", new ImageIcon(image), JLabel.CENTER); // adding the background image to a label
             backgroundLabel.setBorder(BorderFactory.createEmptyBorder(0,0,0,0)); // removing the background from the label that holds the background image of the arena
+
+
+            // Initialize each JLabel object in the racers array, JUST A TEST!!!
+            for (int i = 0; i < maxNumOfRacers; i++) {
+                racersList.add(i, createRacer("src/icons/HelicopterBlack.png"));
+
+                // racersArray[i] = createRacer("src/icons/HelicopterBlack.png");
+                moveRacer(racersList.get(i),0,i*RACER_ICON_SIZE);
+                leftPanel.add(racersList.get(i), BorderLayout.CENTER);
+            }
+            //
+
+            
+            // this line must be in the end for some reason... this might be a problem when adding racers, we will have to write this line after every racer that we add..
             leftPanel.add(backgroundLabel, BorderLayout.CENTER); // adding the label with the image to the left panel of the main screen
 
             mainFrame.setVisible(true); // this line "updates" the main window after we have adding items to it, this way the image is now visible     
